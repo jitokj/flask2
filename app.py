ccbdf597ef973c2d383b89d7295fce3b43b1e553
jitokj <44109840+jitokj@users.noreply.github.com>
2018-10-20 14:58:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,request
+from data import  Faculties
+app=Flask(__name__)
+
+
+getFaculties=Faculties()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')  
+
+@app.route('/send', methods=['GET','POST'])
+def send():
+     if (request.method=='POST'):
+            getname=request.form['name']
+            getemail=request.form['email']
+     return render_template('contact.html',name=getname,email=getemail)
+
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html',sendFaculties=getFaculties)     
+if(__name__=='__main__'):
+     app.run(debug=True)
+
+    
+
+   
+   
+   
+    
---
+++ data.py
@@ -0,0 +1,24 @@
+def Faculties():
+    faculties=[
+        {
+        'dept':'computer science ',
+        'name':'Jito Joy',
+        'MOB' :9745026998,
+        'email':'jitokj3@gmail.com'
+    },
+    {
+        'dept':'Civil Engg ',
+        'name':'RAMYA R',
+        'MOB' :9745056768,
+        'email':'ramyar@gmail.com'
+
+    },
+    {
+        'dept':'Electrical Engg ',
+        'name':'Ramesh Ram',
+        'MOB' :7907056768,
+        'email':'ramesh93@gmail.com'
+    }
+    ]
+    return faculties
+
